47b4c1c80abe36b1d65f2642cfb9cac5844c1f69
truong <vuquangtruong271@gmail.com>
2020-12-26 10:19:40 +0700

    ss

---
+++ src/main/java/com/example/assignment_ead/AssignmentEadApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignment_ead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentEadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentEadApplication.class, args);
+    }
+
+}
